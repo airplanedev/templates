779a888752200fa6acbbcb5a0e9e73b119795b90
Stephen Zhou <stephen@airplane.dev>
2022-09-16 14:34:28 -0400

    Upgrade views version and edit sql

--- user_impersonation_tool/tasks/search_users.sql
+++ user_impersonation_tool/tasks/search_users.sql
@@ -2,13 +2,13 @@ SELECT
     users.id,
     users.name,
     users.email,
-    users.role
+    users.role,
+    users.title
 FROM
     users
 WHERE
     users.name ILIKE :search
     OR users.email ILIKE :search
-    OR users.role ILIKE :search
 ORDER BY
     users.email ASC
 LIMIT 10
