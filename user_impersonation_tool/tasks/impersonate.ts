08e7e35f3a580681d66966c339c60ab490fa7f33
Stephen Zhou <stephen@airplane.dev>
2022-09-16 13:42:20 -0400

    Mock out task data

--- user_impersonation_tool/tasks/impersonate.ts
+++ user_impersonation_tool/tasks/impersonate.ts
@@ -6,17 +6,27 @@ import WorkOS from "@workos-inc/node";
  * The below is an example using WorkOS. Swap out with your auth provider of
  * choice.
  */
-const generateSignInLink = async (userEmail) => {
-  const w = new WorkOS(process.env.WORKOS_API_KEY);
+const generateSignInLink = async (userEmail: string) => {
+  const apiKey = process.env.WORKOS_API_KEY;
+
+  // Return mock data if WORKOS_API_KEY is not set.
+  // Delete this once you create a config variable for WORKOS_API_KEY and set the environment variable.
+  if (apiKey === "") {
+    return { link: "https://airplane.dev" };
+  }
+
+  const w = new WorkOS(apiKey);
   const resp = await w.passwordless.createSession({
     type: "MagicLink",
     email: userEmail,
   });
-  return {
-    link: resp.link,
-  };
-}
+  return { link: resp.link };
+};
 
-export default async function (params) {
+type Params = {
+  user_email: string;
+};
+
+export default async function (params: Params) {
   await generateSignInLink(params.user_email);
 }
