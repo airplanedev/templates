904e334a594644f8f1bda8ac8c5f068b2741f60d
Olushola <olushola251@gmail.com>
2022-11-21 12:39:20 +0100

    basic ruby task

--- ruby_task/entrypoint.sh
+++ ruby_task/entrypoint.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-# Params are in environment variables as PARAM_{SLUG}, e.g. PARAM_USER_ID
-echo "Printing env for debugging purposes:"
-env
+# !/bin/bash
+data_from_ruby=$(ruby "./task.rb")
 
-data='[{"id": 1, "name": "Gabriel Davis", "role": "Dentist"}, {"id": 2, "name": "Carolyn Garcia", "role": "Sales"}]'
-# Show output to users. Documentation: https://docs.airplane.dev/tasks/output#log-output-protocol
-echo "airplane_output_set ${data}"
+echo "airplane_output_set ${data_from_ruby}"
--- ruby_task/task.rb
+++ ruby_task/task.rb
@@ -1,1 +1,1 @@
-puts "Hello from ruby!";
+puts '[{"id": 1, "name": "Gabriel Davis", "role": "Dentist"}, {"id": 2, "name": "Carolyn Garcia", "role": "Sales"}]';
