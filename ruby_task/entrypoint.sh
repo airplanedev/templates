72473ecd0e0a3e16ff56560f6196d382a2e05912
Olushola <olushola251@gmail.com>
2022-11-21 18:14:00 +0100

    ruby script that reads from a sample json file completed

--- ruby_task/entrypoint.sh
+++ ruby_task/entrypoint.sh
@@ -1,3 +1,9 @@
 # !/bin/bash
+
+echo 
+echo This script is being run by $PARAM_ADMIN in $ENVIRONMENT environment
+echo
+echo Deployment platform is $PLATFORM
+
 ruby "./task.rb"
 
--- ruby_task/task.rb
+++ ruby_task/task.rb
@@ -1,8 +1,10 @@
 require "awesome_print"
 require "json"
 
-raw_data = File.read('example.json')
+raw_data = File.read('data.json')
 
+
+puts "\n\nYour JSON file is being parsed pretty printed below \n\n"
 parsed_json = JSON.parse(raw_data)
 
 ap parsed_json
