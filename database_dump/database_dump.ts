3390b3d094a28abea225766cacca7444f471423b
Olushola <olushola251@gmail.com>
2022-11-30 10:47:01 +0100

    Setup flow to upload file

--- database_dump/database_dump.ts
+++ database_dump/database_dump.ts
@@ -1,9 +1,32 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+// import {path} from "path";
+// import {fs} from "fs";
+
 type Params = {
   database_url: string;
 };
 
-// This is your task's entrypoint. When your task is executed, this
-// function will be called.
+const s3Client = new S3Client({
+  region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_KEY || "",
+    secretAccessKey: process.env.AWS_SECRET || "",
+  },
+});
+
 export default async function (params: Params) {
+  try {
+    const data = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET,
+        Key: "",
+        Body: "",
+      })
+    );
+    console.log("SUCEESS", data);
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+
   return {};
 }
