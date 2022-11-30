ce02fd2b0b75b0e0990b24fa13769e38bb5068dd
Olushola <olushola251@gmail.com>
2022-11-30 01:02:59 +0100

    Setup for nodejs databse_dump task

---
+++ database_dump/database_dump.ts
@@ -0,0 +1,9 @@
+type Params = {
+  database_url: string;
+};
+
+// This is your task's entrypoint. When your task is executed, this
+// function will be called.
+export default async function (params: Params) {
+  return {};
+}
